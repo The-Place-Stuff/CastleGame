51291687c3c812b56777fc35ba31093181d36394
Super1166 <110970817+Super1166@users.noreply.github.com>
2024-08-10 21:26:47 +0300

    Health component

---
+++ src/gameobjects/components/misc/Health.cs
@@ -0,0 +1,45 @@
+﻿using SerpentEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastleGame
+{
+    public class Health : Component
+    {
+        public float Points { get; set; } = 0;
+
+        public float Size { get; set; } = 0;
+        public Health(int size) : base(false)
+        {
+            Size = size;
+        }
+
+
+        public void Decrement()
+        {
+            Points--;
+        }
+
+        public void Decrement(float size)
+        {
+            Points -= size;
+        }
+
+        public void Increment()
+        {
+            Points++;
+        }
+        public void Increment(float size)
+        {
+            Points += size;
+        }
+
+        public bool IsEmpty()
+        {
+            return Points == 0;
+        }
+    }
+}
