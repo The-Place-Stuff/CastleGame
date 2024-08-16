95e691e990871ad3d2315f428cc851981f2cf075
Super1166 <110970817+Super1166@users.noreply.github.com>
2024-08-17 00:35:33 +0300

    The sheep isin

---
+++ src/gameobjects/characters/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastleGame
+{
+    public class Animal : Character
+    {
+        public Animal(string name, float maxHealth, float speed, int range) : base(name, maxHealth, speed, range)
+        {
+        }
+
+    }
+}
