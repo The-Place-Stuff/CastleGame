886f4829d394901ac7f83dad892bb9467a973fa3
Super1166 <110970817+Super1166@users.noreply.github.com>
2024-08-28 02:29:15 +0300

    Button test

---
+++ src/gameobjects/ui/CastleGameButton.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using SerpentEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastleGame
+{
+    public class CastleGameButton : GameObject
+    {
+
+        public override void Load()
+        {
+
+            NineSliceSprite nineSliceSprite = new NineSliceSprite("assets/img/nineslice");
+            Position = new Vector2(70, 70);
+            nineSliceSprite.Size = new Vector2(16, 16);
+            nineSliceSprite.SetPadding(2);
+            AddComponent(nineSliceSprite);
+
+            Button button = new Button(new Vector2(16, 16));
+            AddComponent(button);
+
+            button.OnClick += OnClick;
+
+            base.Load();
+        }
+
+
+        public void OnClick(Vector2 targetPosition)
+        {
+            DebugGui.Log("Clicked at " + targetPosition);
+        }
+    }
+}
