cdf8bbb078bbb4fbf6c41c5c4ad33e17b4c4d9b3
Warhead <warhead51707xbox@gmail.com>
2024-09-08 15:31:33 -0400

    Update Drops.cs

--- src/registries/Drops.cs
+++ src/registries/Drops.cs
@@ -10,14 +10,15 @@ namespace CastleGame
 {
     public class Drops : Registry
     {
-        public static new Dictionary<string, Drop> List = new Dictionary<string, Drop>();
+        public static new Dictionary<string, Func<Drop>> List = new Dictionary<string, Func<Drop>>();
 
-        public static readonly Drop Tree = Register(new Drop(new Drop.Settings().AddDrop(Items.Wood(), 3).SetSource(Objects.Tree())));
-        public static readonly Drop Rock = Register(new Drop(new Drop.Settings().AddDrop(Items.Stone()).SetSource(Objects.Rock())));
+        public static readonly Func<Drop> Tree = Register(() => new Drop(new Drop.Settings().AddDrop(Items.Wood(), 3).SetSource(Objects.Tree())));
+        public static readonly Func<Drop> Rock = Register(() => new Drop(new Drop.Settings().AddDrop(Items.Stone()).SetSource(Objects.Rock())));
 
-        public static Drop Register(Drop drop)
+        public static Func<Drop> Register(Func<Drop> drop)
         {
-            List.Add(drop.DropSettings.Source.Name, drop);
+            List.Add(drop().DropSettings.Source.Name, drop);
+
             return drop;
         }
 
@@ -31,7 +32,7 @@ namespace CastleGame
         {
             if (!List.ContainsKey(name)) return null;
 
-            return List[name];
+            return List[name]();
         }
     }
 }
