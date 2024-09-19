c36065119c9299363f6f33e809fcc5247ecb50bf
ExDrill <72329798+ExDrill@users.noreply.github.com>
2024-09-18 22:34:40 -0500

    First attempt on Connectable

---
+++ src/gameobject/component/connectable/ConnectableModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CastleGame;
+
+public class ConnectableModel
+{
+    [JsonPropertyName("spritesheet")]
+    public string SpriteSheet { get; set;}
+
+    [JsonPropertyName("frame_size")]
+    public Vector2 FrameSize { get; set; }
+
+    [JsonPropertyName("default_offset")]
+    public Vector2 DefaultOffset { get; set; }
+
+    [JsonPropertyName("conditions")]
+    public List<Condition> Conditions { get; set; }
+
+    public class Condition
+    {
+        [JsonPropertyName("search_directions")]
+        public List<string> SearchDirections { get; set; }
+
+        [JsonPropertyName("offset")]
+        public Vector2 Offset { get; set; }
+    }
+}
